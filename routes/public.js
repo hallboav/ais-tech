b0c8ed8287aebc0c00f4645adafa09743699d14e
Hallison Boaventura <hallisonboaventura@gmail.com>
2017-05-09 00:40:54 -0300

    querying user in mongodb

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,30 @@
+const User = require('../models/user');
 const router = require('express').Router();
 
 router.post('/login', function (req, res, next) {
     const body = req.body;
 
-    /**
-     * @todo add json web token
-     */
-    res.json({ authorization: 'token' });
+    User.findOne({ email: body.email }, function (err, user) {
+        if (err) throw err;
+
+        if (!user) {
+            res.status(404);
+            return res.json();
+        }
+
+        /**
+         * @todo add bcrypt password encoder
+         */
+        if (body.password !== user.password) {
+            res.status(401);
+            return res.json();
+        }
+
+        /**
+         * @todo add json web token
+         */
+        res.json({ authorization: 'token' });
+    });
 });
 
 module.exports = router;
